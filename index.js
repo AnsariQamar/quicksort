f61d561be76ff61fd98941aa5c734983fb9d8f17
AnsariQamar <ssky5021@gmail.com>
2022-04-01 22:04:29 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-function sort(arr){
-  let left=[];
-  let right=[];
-   let temp=[];
-  let pivotIdx=0;
-  let pivotValue=arr.shift();
-  temp[pivotIdx]=pivotValue;
-  if(arr.length==0){
+function sort(arr) {
+  let left = [];
+  let right = [];
+  let temp = [];
+  let pivotIdx = 0;
+  let pivotValue = arr.shift();
+  temp[pivotIdx] = pivotValue;
+  if (arr.length == 0) {
     return temp;
   }
-  for(let i=0; i<arr.length; i++){
-    if(parseInt(arr[i])<pivotValue){
+  for (let i = 0; i < arr.length; i++) {
+    if (parseInt(arr[i]) < pivotValue) {
       left.push(arr[i]);
-    }else if(parseInt(arr[i])>=pivotValue){
+    } else if (parseInt(arr[i]) >= pivotValue) {
       right.push(arr[i]);
     }
   }
-  const ans=sort(left).concat(temp,sort(right));
-  if(ans[0]==undefined){
+  const ans = sort(left).concat(temp, sort(right));
+  if (ans[0] == undefined) {
     ans.shift();
   }
-  if(ans[ans.length-1]==undefined){
+  if (ans[ans.length - 1] == undefined) {
     ans.pop();
   }
-return ans;
+  return ans;
 }
 
-const a=sort([1,8,5,7,2,-1,0]);
+const a = sort([1, 8, 5, 7, 2, -1, 0]);
 console.log(a);
